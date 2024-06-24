043347d76a0b9e1b513b0617debea6f14ca3852b
unknown <prithiviraina1998@gmail.com>
2024-06-24 16:46:35 +0530

    longest palindrome

---
+++ src/main/java/org/task/interview/Palindrome.java
@@ -0,0 +1,46 @@
+package src.main.java.org.task.interview;
+
+public class Palindrome {
+
+    private int expandAroundCenter(String input, int left, int right) {
+
+        while (left >= 0 && right < input.length() && input.charAt(left) == input.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+
+    private String longestPalindrome(String input) {
+
+        if (null == input || input.length() < 1) return "";
+
+        if (input.length() == 1) return input;
+
+        int start = 0;
+        int subLength = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            int l1 = expandAroundCenter(input, i, i);
+            int l2 = expandAroundCenter(input, i, i + 1);
+            int length = Math.max(l1, l2);
+
+            if (length > subLength) {
+                start = i - (length - 1) / 2;
+                subLength = length;
+            }
+        }
+
+        return input.substring(start, start + subLength);
+    }
+
+    public static void main(String[] args) {
+
+        Palindrome palindrome = new Palindrome();
+
+        String ls = palindrome.longestPalindrome("caba");
+        System.out.println("LONGEST SUB STRING " + ls);
+
+    }
+}
